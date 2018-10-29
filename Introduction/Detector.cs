1fc86e943b577a0cec7273ff546b1544fdc1efee
Michael <panmihaurkin@icloud.com>
2018-10-29 17:02:36 +0700

    Gaussian blur method added

---
+++ Introduction/Detector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using static Introduction.Data;
+
+namespace Introduction
+{
+    public class Detector
+    {
+        public Image<Gray, byte> GaussianBlur(Image<Bgr, byte> img, int radius)
+        {
+            Image<Gray, byte> grayImage = img.Convert<Gray, byte>();
+            Image<Gray, byte> bluredImage = grayImage.SmoothGaussian(radius);            return bluredImage ?? throw new ArgumentNullException(paramName: nameof(bluredImage));        }
+
+
+    }
+}
